cf9c4b7cf7727c24985c695e2657b9bb51088298
Sohel9146 <suhailshaikh7866@gmail.com>
2025-08-28 16:03:23 +0530

    Updated & Added Some Extra Things

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders React CI/CD Demo text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/React CI\/CD Demo/i);
   expect(linkElement).toBeInTheDocument();
 });
+
